4c019913c954b44639b720fa197b0c398cd5a758
Ulil Albab <aoohan@Ulils-MacBook-Pro.local>
2022-07-02 17:02:48 +0700

    get detail data

--- src/pages/DetailMakanan/[id].tsx
+++ src/pages/DetailMakanan/[id].tsx
@@ -25,16 +25,14 @@ type StateData = {
 
 const DetailMakanan = () => {
     const router = useRouter();
-    console.log(router.query.id);
     const id_item = router.query.id
     const data = useSelector((state: {food: Food[]}) => state.food);
-    const data_select = data.findIndex((id) => id === id_item)
-    console.log(data_select);
+    const data_select = data.findIndex((item) => item.id === id_item)
     return (
         <div className="h-[960px]">
             <div className="bg-white">
                 <Head>
-                    <title>Detail {data_select?.type}</title>
+                    <title>Detail {data[data_select]?.name}</title>
                 </Head>
                 <div className="z-10 flex bg-cover bg-[url('/image/1.jpg')] h-[375px]">
                     <Link href="/ListMakanan">
@@ -45,27 +43,27 @@ const DetailMakanan = () => {
                 </div>
                 <div className="bg-white h-auto shadow-[0px_-4px_6px_rgba(0,0,0,0.25)] rounded-t-[15px] z-30">
                     <div className="px-7 py-7">
-                        <p className="mb-3 text-xs text-gray-700 dark:text-gray-400">Diposting oleh <b>{data_select?.user}</b></p>
+                        <p className="mb-3 text-xs text-gray-700 dark:text-gray-400">Diposting oleh <b>{data[data_select]?.user}</b></p>
                         <div className="flex items-center gap-2">
-                            <p className="text-xl font-bold">{data_select?.name}</p>
+                            <p className="text-xl font-bold">{data[data_select]?.name}</p>
                             <p className="text-xl">|</p>
-                            <p className="text-sm">{data_select?.type}</p>
+                            <p className="text-sm">{data[data_select]?.type}</p>
                         </div>
                         <div className="flex mt-3">
                             <div className="flex items-center gap-1 p-2 w-auto h-6 text-base text-[12px] font-semibold text-[#431E87] rounded-[25px] outline-1 outline outline-violet-900">
                                 <HiCalendar className="h-4 w-4"></HiCalendar>
-                                <p className="text-[12px]"><b>Exp Date : </b>{data_select?.expiration}</p>
+                                <p className="text-[12px]"><b>Exp Date : </b>{data[data_select]?.expiration}</p>
                             </div>
                         </div>
                         <div className="flex mt-2">
                             <div className="flex items-center p-2 w-auto h-6 text-base text-[12px] font-semibold text-[#431E87] rounded-[25px] outline-1 outline outline-violet-900">
-                                    <h1 className="text-[12px] text-end">Jumlah Tersedia: {data_select?.amount} {data_select?.typeOfAmount}</h1>
+                                    <h1 className="text-[12px] text-end">Jumlah Tersedia: {data[data_select]?.amount} {data[data_select]?.typeOfAmount}</h1>
                             </div>
                         </div>
                         <div className="flex mt-2">
                             <div className="flex items-center gap-1 p-2 w-auto h-6 text-base text-[12px] font-semibold text-[#431E87] rounded-[25px] outline-1 outline outline-violet-900">
                                 <HiLocationMarker className="h-4 w-4"></HiLocationMarker>
-                                <p className="text-xs">{data_select?.address}, {data_select?.city}</p>
+                                <p className="text-xs">{data[data_select]?.address}, {data[data_select]?.city}</p>
                             </div>
                         </div>
                         <div className="mt-2">
@@ -76,7 +74,7 @@ const DetailMakanan = () => {
                             </div>
                         </div>
                         <div className="mt-1">
-                            <p className="text-[12px]">{data_select?.description}</p>
+                            <p className="text-[12px]">{data[data_select]?.description}</p>
                         </div>
                         <div className="mt-[5px]">
                             <div className="relative flex py-5 items-center">
@@ -84,7 +82,7 @@ const DetailMakanan = () => {
                             </div>
                         </div>
                         <div className="mt-[10px]">
-                            <a href={`https://wa.me/${data_select?.wa}`} target="_blank">
+                            <a href={`https://wa.me/${data[data_select]?.wa}`} target="_blank" rel="noreferrer">
                                 <button className="mr-[5px] px-[18px] py-[5px] w-full hover:bg-emerald-300  bg-white  text-base text-[12px] font-semibold text-purple-900 rounded-[25px] outline-1 outline outline-violet-900">
                                     Hubungkan dengan WA
                                 </button>
