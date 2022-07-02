b295b43af909883e14f5b92f5bb783d3120fab2f
Lathifa Itqon <lathifaitqon@gmail.com>
2022-07-02 10:19:10 +0700

    update: style component card item

--- src/components/CardItem/index.tsx
+++ src/components/CardItem/index.tsx
@@ -34,14 +34,14 @@ const CardItem = ({
                 <img className="object-cover rounded-none rounded-l-lg w-[150px] h-full" src={food.id.length === 1 || food.id.length == 2  ? image : `/image/${Math.floor(Math.random() * (10 - 1 + 1)) + 1}.jpg`} alt=""/>
                 <div className="flex flex-col justify-between leading-normal p-3">
                     <p className="mb-2 text-md font-bold tracking-tight text-gray-500 dark:text-purple-900">{name}</p>
-                    <p className="mb-3 text-xs text-gray-700 dark:text-gray-400">Diposting oleh <b>Lathifa Itqonina</b></p>
+                    <p className="mb-3 text-xs text-gray-700 dark:text-gray-400 text-[10px]">Diposting oleh <b>Lathifa Itqonina</b></p>
                     <div className="flex grid-cols-2 mb-3">
                         <HiLocationMarker></HiLocationMarker> 
-                        <p className="ml-1 text-xs font-bold dark:text-black">{address}, {city}</p>
+                        <p className="ml-1 text-xs font-semibold dark:text-black text-[10px]">{address}, {city}</p>
                     </div>
                     <div className="flex grid-cols-2">
                         <HiCalendar></HiCalendar> 
-                        <p className="ml-1 text-xs font-bold dark:text-black"> Exp: {expiration}</p>
+                        <p className="ml-1 text-xs font-semibold dark:text-black text-[10px]"> Exp: {expiration}</p>
                     </div>
                 </div>
             </a>
