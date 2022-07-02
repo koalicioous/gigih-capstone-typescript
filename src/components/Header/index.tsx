f7791dd6da53bfc90ad36a707e737ae6e0d823c1
Lathifa Itqon <lathifaitqon@gmail.com>
2022-07-03 02:13:40 +0700

    update: header view

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,14 +8,14 @@ const Header = () : JSX.Element => {
     const id_item = router.query.id
     return (
         <div className="bg-purple-900 sticky top-0 z-[99]">
-            <nav className="bg-purple-900 drop-shadow-[0_3px_5px_rgba(0,0,0,0.15)]">
-                <div className="flex justify-center">
+            <nav className="bg-purple-900 drop-shadow-[0_3px_3px_rgba(0,0,0,0.25)]">
+                <div className="flex items-center">
                     <Link href="/ListMakanan">
                         <button className="position-relative text-base text-[25px] font-semibold text-white rounded-[25px] outline-1 outline-none">
                             <HiOutlineArrowLeft className="ml-8" style={ id_item ? { display:'block'} : {display : 'none'} }/>
                         </button>
                     </Link>
-                    <form className="flex items-center w-full px-8 py-4 my-2">   
+                    <form className="flex items-center w-full px-8 py-4 my-2"  style={ id_item ? { display:'none'} : {display : 'block'} }>   
                         <label htmlFor="search" className="sr-only">Search</label>
                         <div className="relative w-full">
                             <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
@@ -24,9 +24,9 @@ const Header = () : JSX.Element => {
                             <input type="text" id="search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-2xl focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-1.5" autoComplete="off" placeholder="Search Food ..." />
                         </div>
                     </form>
-                    {/* <a className="flex flex-col items-center justify-center no-underline text-teal-dark ml-8 mr-5 transition-colors duration-100 ease-in-out focus:text-purple-900" href="#">
-                        <span className="font-serif text-xl text-white tracking-wide">Pesanan.</span>
-                    </a> */}
+                    <a className="flex flex-col items-center justify-center no-underline text-teal-dark ml-5 mr-5 transition-colors duration-100 ease-in-out focus:text-purple-900 my-6" href="#" style={ id_item ? { display:'block'} : {display : 'none'} }>
+                        <span className="text-xl text-white tracking-wide font-semibold">Detail Item</span>
+                    </a>
                 </div>
             </nav>
         </div>
